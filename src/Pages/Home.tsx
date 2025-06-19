da28ac45896a1f3b3b3ae744c039c59b0218f7b2
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-06-19 20:58:48 +0200

    Add functionality to render random note

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,6 +1,10 @@
 import StaveComponent from "../components/StaveComponent";
 
 export default function Home() {
+  const notes: string[] = ["c", "d", "e", "f", "g", "a", "h", "c2"];
+  const noteType: string = notes[Math.floor(Math.random() * notes.length)];
+  console.log("Note type:", noteType);
+
   return (
     <>
       <StaveComponent />
