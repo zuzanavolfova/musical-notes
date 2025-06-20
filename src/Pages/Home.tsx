53469f81d723ad7d4954856797e6050c0e41c8fe
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-06-20 09:24:04 +0200

    Add select buttons to hoem view

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,6 +1,6 @@
 import StaveComponent from "../components/StaveComponent";
 import NoteComponent from "../components/NoteComponent";
-
+import SelectButton from "../components/Buttons/SelectButton";
 export default function Home() {
   const notes: string[] = ["c", "d", "e", "f", "g", "a", "h", "c2"];
 
@@ -12,13 +12,22 @@ export default function Home() {
 
   return (
     <>
-      <div style={{ position: "relative", width: "100%" }}>
+      <div
+        style={{
+          position: "relative",
+          height: "140px",
+          margin: "20px",
+        }}
+      >
         <StaveComponent />
         <NoteComponent
           noteImage={noteType === "c" ? "note2" : "note"}
           noteType={noteType}
         />
       </div>
+      {notes.map((note) => (
+        <SelectButton key={note} answerText={note} />
+      ))}
     </>
   );
 }
