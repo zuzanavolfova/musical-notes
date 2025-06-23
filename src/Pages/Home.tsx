ed24789e24df68f3291b52dd7922715b54a4dd00
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-06-23 13:29:36 +0200

    Edit margin of result text

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -97,6 +97,7 @@ export default function Home() {
         style={{
           color: answerResult ? "var(--success-color)" : "var(--wrong-color)",
           fontSize: "32px",
+          margin: "20px auto 0 auto",
         }}
       >
         {answerResult != null && (
