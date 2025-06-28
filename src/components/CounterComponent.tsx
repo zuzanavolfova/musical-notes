82106ee76c99fb55ca9dffcf3219df28978d7065
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-06-28 10:53:53 +0200

    Add counter component

---
+++ src/components/CounterComponent.tsx
@@ -0,0 +1,47 @@
+import { styled } from "styled-components";
+import { useTranslation } from "react-i18next";
+const CounterStyled = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin: 8px 0;
+  .amount {
+    &__counter {
+      margin-left: 8px;
+    }
+  }
+`;
+
+export default function CounterComponent() {
+  const { t } = useTranslation();
+  const wrongAnswers = 0;
+  const goodAnswers = 0;
+
+  return (
+    <CounterStyled aria-labell="Amount of answers">
+      <div className="amount__container" aria-labell="Amount of good answers">
+        <span className="amount__label" id="good-answers-label">
+          {t("Amount-good-answers")}
+        </span>
+        <span
+          className="amount__counter"
+          aria-labelledby="good-answers-label"
+          aria-live="polite"
+        >
+          {goodAnswers}
+        </span>
+      </div>
+      <div className="amount__container" aria-labell="Amount of good answers">
+        <span className="amount__label" id="wrong-answers-label">
+          {t("Amount-wrong-answers")}
+        </span>
+        <span
+          className="amount__counter"
+          aria-labelledby="wrong-answers-label"
+          aria-live="polite"
+        >
+          {wrongAnswers}
+        </span>
+      </div>
+    </CounterStyled>
+  );
+}
