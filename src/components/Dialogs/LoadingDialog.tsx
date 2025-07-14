fb1785c47aa51bc833e9b5166b8cfd555f546a32
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-07-14 09:01:27 +0200

    Fox loading dialog style

--- src/components/Dialogs/LoadingDialog.tsx
+++ src/components/Dialogs/LoadingDialog.tsx
@@ -2,19 +2,20 @@ import { useTranslation } from "react-i18next";
 import { styled } from "styled-components";
 
 import Dialog from "./Dialog";
-
 import type { LoadingProps } from "../../types/interfaces";
 
-const LoadingDialogStyled = styled.div`
+const LoadingContent = styled.div`
   display: flex;
   flex-direction: column;
-  align-content: center;
+  align-items: center;
   justify-content: center;
+
   .loading__text {
     margin: 20px;
     text-align: center;
   }
 `;
+
 const Spinner = styled.div`
   width: 40px;
   height: 40px;
@@ -22,7 +23,6 @@ const Spinner = styled.div`
   border-top: 4px solid var(--primary-color-hover);
   border-radius: 50%;
   animation: spin 1s linear infinite;
-  margin: auto;
 
   @keyframes spin {
     0% {
@@ -36,12 +36,13 @@ const Spinner = styled.div`
 
 export default function LoadingDialog({ message = "loading" }: LoadingProps) {
   const { t } = useTranslation();
+
   return (
-    <LoadingDialogStyled>
-      <Dialog showHeader={false} disableOutsideClick disableEsc>
+    <Dialog size="S" showHeader={false} disableOutsideClick disableEsc>
+      <LoadingContent>
         <span className="loading__text">{t(message)}</span>
         <Spinner />
-      </Dialog>
-    </LoadingDialogStyled>
+      </LoadingContent>
+    </Dialog>
   );
 }
