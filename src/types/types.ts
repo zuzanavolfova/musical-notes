e7a7aaff91fc7b70091685468f8054f00fe649c9
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-07-17 09:49:57 +0200

    Add user name to user context

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,6 +5,7 @@ export type UserContextType = {
   registerDialogOpen: boolean;
   userManagementDialogOpen: boolean;
   setUser: (user: string) => void;
+  setUserName: (user: string) => void;
   setIsLogin: (open: boolean) => void;
   setIsLogInOpen: (open: boolean) => void;
   setIsRegisterOpen: (open: boolean) => void;
